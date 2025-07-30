0de50a58524855e2db4cdfbbece1544bdcef6ec9
cuongpd <phancuongtmtlhp@gmail.com>
2025-07-30 05:12:36 +0000

    push setting config

---
+++ script/setting_github.sh
@@ -0,0 +1,7 @@
+git remote add git@github.com:AE-AI-HIT15/hit-product-group-2.git
+
+git branch -m main
+git config --global user.email "hungmanh.aitech@gmail.com"
+git config --global user.name "hunghm"
+
+cat ~/.ssh/id_rsa.pub
---
+++ script/setting_monitor.sh
@@ -0,0 +1,3 @@
+sudo apt install htop
+
+sudo apt install nvidia-utils-515 -y
